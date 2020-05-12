8013c48c127b09ec3f8af5a382b94998e9c96f94
Margret `Pax` Williams <pax.margret@gmail.com>
2020-05-12 07:14:38 -0700

    :truck: Move components to a separate package

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,5 +1,5 @@
 import 'dart:io' show Platform;
-import 'package:bitcoin_ticker/conversion_card.dart';
+import 'package:bitcoin_ticker/components/conversion_card.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
